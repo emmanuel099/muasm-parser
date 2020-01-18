6deb309e22dc75465a4df7f44edff65ea0ab9206
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2020-01-18 19:04:01 +0100

    Make main a simple parser application which takes an filename as input and prints the resulting IR

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use muasm_parser::parser;
+use std::env;
+use std::fs;
 
 fn main() {
-    let src = r#"
-        cond <- x < array1_len
-        beqz cond, EndIf
-    Then:
-        load v, array1 + x
-        load tmp, array2 + v << 8
-    EndIf:"#;
-
-    let program = parser::parse_program(src).unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Expected a filename as input");
+    }
+    let filename = &args[1];
+    let src = fs::read_to_string(filename).expect("Could not read file");
+    let program = parser::parse_program(&src).expect("Could not parse file");
     println!("{}", program);
 }
